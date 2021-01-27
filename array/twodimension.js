bb235e03dfa4e15087be3a3a9355ac2d7c94cae2
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-01-27 10:28:45 +0630

    #add: inserting value in 2D array

--- array/twodimension.js
+++ array/twodimension.js
@@ -18,7 +18,7 @@ arr = create2DArray(2, 3); // O(1)
 arr[0][0] = 0; // O(1) ------
 arr[0][1] = 1; // O(1)      -
 arr[0][2] = 2; // O(1)      -
-arr[1][0] = 3; // O(1)      ---- O(mn)
+arr[1][0] = 3; // O(1)      ---- O(n^2)
 arr[1][1] = 4; // O(1)      -
 arr[1][2] = 5; // O(1)-------
 
@@ -31,3 +31,15 @@ let arr2 = [
 ];  // O(1)
 
 console.table(arr2);
+
+// insertion 
+let arr3 = [[],[]];
+function insert(arr, val, row, col){
+    if(arr[row][col]){ // O(1)
+        return "This cell is already in use"; // O(1)
+    }
+    arr[row][col] = val; // O(1)
+}
+
+insert(arr3,0,1,2);
+console.table(arr3);
