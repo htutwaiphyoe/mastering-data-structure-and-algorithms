e60215c036cd8ec3bb29a939e84c98fd32b1e850
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-01-30 14:53:56 +0630

    #add: single linkedlist insertion

--- linkedlist/SingleLinkedlist.js
+++ linkedlist/SingleLinkedlist.js
@@ -1,8 +1,70 @@
+const Node = require("./Node");
+
 class SingleLinkedlist {
     constructor() {
         this.size = 0;
         this.head = null;
         this.tail = null;
+    }
+    prepend(value) {
+        // create new node, store value and store next
+        const node = new Node(value, this.head);
+
+        // update head with new node
+        this.head = node;
+
+        // check tail is null and update tail with new node
+        if (!this.tail) this.tail = node;
+
+        // increment list size
+        this.size += 1;
+
+        return this;
+    }
+    append(value) {
+        // create new node and store value
+        const node = new Node(value);
+
+        // update last node next with new node
+        this.tail.next = node;
+
+        // update tail with new node
+        this.tail = node;
+
+        // increment list size
+        this.size += 1;
+
+        return this;
+    }
+    insert(value, index) {
+        // check index is valid
+        if (index < 0 || index > this.size) {
+            return this;
+        }
+
+        // if index is 0, call prepend
+        if (index === 0) {
+            return this.prepend(value);
+        }
+
+        // create new node
+        const node = new Node(value);
+
+        let cur, prev;
+        cur = this.head;
+        let i = 0;
+
+        while (i < index) {
+            i++;
+            prev = cur;
+            cur = cur.next;
+        }
+
+        node.next = cur;
+        prev.next = node;
+        
+        this.size++;
+        return this;
     }
 }
 
--- linkedlist/index.js
+++ linkedlist/index.js
@@ -1,2 +1,10 @@
-const Node = require("./Node");
 const SingleLinkedlist = require("./SingleLinkedlist");
+
+const singleLinkedList = new SingleLinkedlist();
+
+singleLinkedList.insert(20, 0);
+singleLinkedList.prepend(10);
+singleLinkedList.append(40);
+singleLinkedList.insert(30, 2);
+
+console.log(singleLinkedList.head);
