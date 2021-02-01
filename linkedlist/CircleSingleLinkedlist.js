c7c3d1b629e99a28f70bff221da2f375830fc470
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-02-01 17:12:31 +0630

    insertion first node in circular single linkedlist

--- linkedlist/CircleSingleLinkedlist.js
+++ linkedlist/CircleSingleLinkedlist.js
@@ -1,7 +1,24 @@
+const Node = require("./Node");
+
 class CircleSingleLinkedList {
-    constructor(){
+    constructor() {
         this.size = 0;
         this.head = null;
         this.tail = null;
     }
+    check() {
+        if (!this.head) {
+            return false;
+        }
+        return true;
+    }
+    prepend(val) {
+        const node = new Node(val, this.head);
+        this.head = node;
+        this.size === 0 ? (this.tail = this.head) : (this.tail.next = this.head);
+        this.size++;
+        return this;
+    }
 }
+
+module.exports = CircleSingleLinkedList;
--- linkedlist/index.js
+++ linkedlist/index.js
@@ -1,1 +1,9 @@
 const CircleSingleLinkedList = require("./CircleSingleLinkedList");
+
+const csll = new CircleSingleLinkedList();
+
+csll.prepend(50);
+csll.prepend(100);
+
+console.log(csll.head);
+
