8bd1030e00317647a8a12b024ad6772b289931bc
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-02-01 17:25:43 +0630

    insertion last node in circular single linkedlist

--- linkedlist/CircleSingleLinkedlist.js
+++ linkedlist/CircleSingleLinkedlist.js
@@ -13,9 +13,32 @@ class CircleSingleLinkedList {
         return true;
     }
     prepend(val) {
-        const node = new Node(val, this.head);
-        this.head = node;
-        this.size === 0 ? (this.tail = this.head) : (this.tail.next = this.head);
+        const node = new Node(val);
+
+        if (this.size === 0) {
+            this.head = node;
+            node.next = this.head;
+            this.tail = this.head;
+        } else {
+            node.next = this.head;
+            this.head = node;
+            this.tail.next = this.head;
+        }
+
+        this.size++;
+        return this;
+    }
+    append(val) {
+        const node = new Node(val);
+        if (this.size === 0) {
+            this.head = node;
+            node.next = this.head;
+            this.tail = this.head;
+        } else {
+            node.next = this.head;
+            this.tail.next = node;
+            this.tail = node;
+        }
         this.size++;
         return this;
     }
--- linkedlist/index.js
+++ linkedlist/index.js
@@ -2,8 +2,14 @@ const CircleSingleLinkedList = require("./CircleSingleLinkedList");
 
 const csll = new CircleSingleLinkedList();
 
-csll.prepend(50);
+csll.prepend(200);
 csll.prepend(100);
 
 console.log(csll.head);
+
+csll.append(300);
+csll.append(400);
+
+console.log(csll.head);
+
 
